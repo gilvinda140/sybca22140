fa10e6604f65932b2ba65d6d9b253811ee240604
gilvinda140 <gilvindadsouza303@gmail.com>
2024-02-05 10:29:30 +0530

    commit

---
+++ gilvinda/src/gilvinda/catiplier140.java
@@ -0,0 +1,14 @@
+package gilvinda;
+
+public class catiplier140 {
+
+	
+		int moves,cabbages;
+		catiplier140(int moves,int cabbages)
+		{
+			this.moves=moves;
+			this.cabbages=cabbages;
+		}
+	
+
+}
